70ccc9cc0f812aa7667adc0674f665e025813598
Taofiq Badmus <tbadmus1975@gmail.com>
2019-10-15 11:04:15 -0400

    update variables in file

--- custodian.tf
+++ custodian.tf
@@ -33,17 +33,17 @@ resource "template_dir" "policy" {
   destination_dir = "${path.cwd}/policies"
 
   vars = {
-    cc_role                      = "${aws_iam_role.cc_role.arn}"
+    cc_role                      = ${aws_iam_role.cc_role.arn}"
     cc_sqs                       = "${aws_sqs_queue.cc_queue.id}"
-    cc_schedule                  = "${var.schedule}"
-    cc_excluded_tag              = "${var.excluded_tag}"
-    cc_excluded_value            = "${var.excluded_value}"
-    recipient                    = "${var.recipient}"
-    sender                       = "${var.sender}"
-    key_expiration_template      = "${var.key_expiration_template}"
-    mfa_false_template           = "${var.mfa_false_template}"
-    password_expiration_template = "${var.password_expiration_template}"
-    temp_pass_template           = "${var.temp_pass_template}"
+    cc_schedule                  = var.schedule
+    cc_excluded_tag              = var.excluded_tag
+    cc_excluded_value            = var.excluded_value
+    recipient                    = var.recipient
+    sender                       = var.sender
+    key_expiration_template      = var.key_expiration_template
+    mfa_false_template           = var.mfa_false_template
+    password_expiration_template = var.password_expiration_template
+    temp_pass_template           = var.temp_pass_template
   }
 }
 
@@ -109,7 +109,7 @@ resource "aws_lambda_function" "sqs_mailer" {
   source_code_hash = "${base64sha256("lambda/sqs_mailer.zip")}"
   environment {
     variables = {
-      sender = "${var.sender}"
+      sender = var.sender
     }
   }
 }
